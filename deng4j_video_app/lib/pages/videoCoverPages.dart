4523aafd1ae1db8a8facca62bbb00da23160bfaa
邓志鸿 <1353976012@qq.com>
2024-09-24 02:21:31 +0800

    23

--- deng4j_video_app/lib/pages/videoCoverPages.dart
+++ deng4j_video_app/lib/pages/videoCoverPages.dart
@@ -20,6 +20,9 @@ class VideoCoverPages extends StatefulWidget {
 class _VideoCoverPagesState extends State<VideoCoverPages> {
   late AjaxResult _ajaxResult;
 
+  // 滚动控制器
+  ScrollController _scrollController = ScrollController();
+
   void _getCoverList() {
     Category category = widget.category;
     dataCounterCounterPublic.categoryVideoVOList.forEach((e) {
@@ -33,22 +36,69 @@ class _VideoCoverPagesState extends State<VideoCoverPages> {
   void initState() {
     super.initState();
     _getCoverList();
+
+    // 为滚动控制器添加监听
+    _scrollController.addListener(() {
+      // _scrollController.position.pixels 是当前像素点位置
+      // _scrollController.position.maxScrollExtent 当前列表最大可滚动位置
+      // 如果二者相等 , 那么就触发上拉加载更多机制
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        // 触发上拉加载更多机制
+        _loadMore();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // 销毁 滚动控制器 ScrollController
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return KeepAliveWrapper(
-        // 页面缓存
-        child: GridView.count(
-      //水平子 Widget 之间间距
-      crossAxisSpacing: 3.0,
-      //垂直子 Widget 之间间距
-      mainAxisSpacing: 10.0,
-      //一行的 Widget 数量
-      crossAxisCount: 3,
-      //宽度和高度的比例,0.735
-      childAspectRatio: 0.7,
-      children: _ajaxResult.widgetList,
-    ));
+      // 页面缓存
+      child: RefreshIndicator(
+          // 设置下拉刷新组件
+          onRefresh: _onRefresh,
+          child: GridView.count(
+            controller: _scrollController,
+            //水平子 Widget 之间间距
+            crossAxisSpacing: 3.0,
+            //垂直子 Widget 之间间距
+            mainAxisSpacing: 10.0,
+            //一行的 Widget 数量
+            crossAxisCount: 3,
+            //宽度和高度的比例,0.735
+            childAspectRatio: 0.7,
+            children: _ajaxResult.widgetList,
+          )),
+    );
+  }
+
+  /// 上拉加载更多
+  _loadMore() async {
+    /// 强制休眠 1 秒
+    await Future.delayed(Duration(seconds: 1));
+
+    /// 更新 UI
+    setState(() {
+      print("上拉加载更多");
+    });
+  }
+
+  /// 下拉刷新回调方法
+  Future<Null> _onRefresh() async {
+    /// 强制休眠 1 秒
+    await Future.delayed(Duration(seconds: 1));
+
+    /// 更新状态
+    setState(() {
+      print("下拉刷新回调方法");
+    });
+    return null;
   }
 }
